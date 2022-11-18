0d717ef24b14ca5e51150a594b8206ee2391a8e2
thiberte <thiberte@educ.college-montreal.qc.ca>
2022-11-18 16:09:11 -0500

    commit initial.

--- TP3.py
+++ TP3.py
@@ -6,25 +6,51 @@ Faire un combat de monstre
 import random
 import time
 point_de_vie = 20
+victoire = 0
+defaites = 0
+numero_adversaire = 0
+numero_combat = 0
 force_ennemi = random.randint(1, 5)
 
 play_game = True
 while play_game:
-    print("Vous tombez face a face avec un ennemi de niveau", force_ennemi,)
-    choix_premiere_porte = int(input("Que voulez vous faire\n"
+    print("Vous tombez face a face avec un ennemi")
+    choix_porte = int(input("Que voulez vous faire\n"
                                      "1 - L'attaquer\n"
                                      "2 - Le contourner et aller ouvrir une porte\n"
                                      "3 - Afficher les règles du jeu\n"
                                      "4 - Quitter la partie\n"))
-    if choix_premiere_porte == 1:
+    if choix_porte == 1: #attaquer le monstre
+        numero_adversaire = numero_adversaire + 1;
+        print("Ennemi: #", numero_adversaire, )
+        print("Force de l'ennemi:", force_ennemi, )
+        print("Niveau de vie de l'usager:", point_de_vie, )
+        print("Combat #", numero_combat, ":", victoire, "victoires vs", defaites,"défaites")
         attaque = random.randint(1, 6)
-        if attaque <= 4:
-            print("Vous avez roulé", attaque, "et vous vous êtes fait battre,")
+        if attaque <= 4: #defaite
+            print("Vous vous êtes fait battre,")
             point_de_vie = point_de_vie - 4
-            print("Vous avez Maintenant", point_de_vie, "point de vie\n")
-            time.sleep(1)
-        if attaque >= 5:
-            print("Vous avez roulé", attaque, "et vous avez battu l'ennemi")
-            point_de_vie = point_de_vie + 4
-            print("Vous avez Maintenant", point_de_vie,  "point de vie\n")
-            time.sleep(1)
+            defaites = defaites + 1
+            print("Vous avez maintenant", point_de_vie, "point de vie\n")
+            time.sleep(2)
+        if attaque >= 5: #victoire
+            print("Vous avez battu l'ennemi")
+            point_de_vie = point_de_vie + 4;
+            victoire = victoire + 1;
+            print("Vous avez Maintenant", point_de_vie,  "point de vie")
+            print("Vous avez maintenant", victoire, "victoire\n")
+            time.sleep(2)
+    if choix_porte == 2: #prendre la fuite
+        print("Vous partez, mais vous avez perdu 1 point de vie et vous avez maintenant", point_de_vie,"points de vie")
+        point_de_vie = point_de_vie - 1
+    if choix_porte == 3:
+        print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.\n"
+              "Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
+              "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.\n"
+              "Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
+              "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
+              "L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie\n")
+        time.sleep(2)
+    if choix_porte == 4: #quitter le jeu
+        print("Merci et au revoir...")
+        play_game = False
